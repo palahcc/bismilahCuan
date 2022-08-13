babdd408505359ecef76621ed497e4971d84e8e7
palahcc <90046468+palahcc@users.noreply.github.com>
2022-08-13 10:04:18 -0700

    Delete miner-raptoreum.sh

--- miner-raptoreum.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#
-FOLDER=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-while [ 1 ]; do
-"$FOLDER"/cpuminer-sse2 -a gr -o stratum+tcps://stratum-eu.rplant.xyz:17056 -u WALLET.WORKER_NAME
-sleep 5
-done
