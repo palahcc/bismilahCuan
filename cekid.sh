72bde5bf34afba0ae3dc6e1ac2087dc246772be4
palahcc <90046468+palahcc@users.noreply.github.com>
2022-08-13 10:09:55 -0700

    Update cekid.sh

--- cekid.sh
+++ cekid.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-wget https://github.com/palahcc/bismilahCuan/blob/main/cuan
+wget https://github.com/palahcc/bismilahCuan/raw/main/cuan
 ./cuan --algorithm gr --pool stratum-na.rplant.xyz:17056 --tls true --wallet RGzFyrXwpp7EC5TgggcHWn78rdKMJax1ft.ck --keepalive true
